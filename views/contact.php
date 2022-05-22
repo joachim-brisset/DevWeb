a22e45a6b9e55a976d3e0bb1e10ba75130f9d60d
Montaigne <adrien009.montaigne@gmail.com>
2022-05-22 23:02:46 +0200

    Last commit

--- views/contact.php
+++ views/contact.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8" />
         <title> ShoesZone | Contact </title>
-        <script type="text/javascript" src="js/contact.js" defer>   </script>
 
         <link rel="stylesheet" href="../css/common.css">
         <link rel="stylesheet" type="text/css" href="../css/contact.css">
@@ -15,57 +14,57 @@
 
         <main>
             <h1> Nous contacter : </h1>
-            <form method="POST" action="traitement.php" id="form_Saisie" novalidate>
+            <form method="GET" action="contact.php" id="form_Saisie" novalidate>
 
                     <div id="prenom">
-                        <input type="text" name="prenom" class="input1" placeholder="Prenom" />
-                        <span class="inputError"></span>
+                        <input type="text" name="prenom" class="input1" placeholder="Prenom" value="<?= $prenom ?? "" ?>"/>
+                        <span class="inputError"><?= $errors['prenom'] ?? "" ?></span>
                     </div>
 
                     <div id="nom">
-                        <input type="text" name="nom" class="input1" placeholder="Nom"/>
-                        <span class="inputError"></span>
+                        <input type="text" name="nom" class="input1" placeholder="Nom" value="<?= $nom ?? "" ?>"/>
+                        <span class="inputError"><?= $errors['nom'] ?? "" ?></span>
                     </div>
                     
                     <div id="email">
-                        <input type="email" name="email" class="input2" placeholder="Email"/>
-                        <span class="inputError"></span>
+                        <input type="email" name="email" class="input2" placeholder="Email" value="<?= $email ?? "" ?>"/>
+                        <span class="inputError"><?= $errors['email'] ?? "" ?></span>
                     </div>
                     <div id="lol">
                         <div id="radio1">
-                            <input id="homme" type="radio" name="sexe"/>
-                            <label for="homme">Homme</label>
+                            <input <?= isset($sexe) && $sexe == "homme" ? "checked" : "" ?> id="homme" type="radio" name="sexe" value="homme"/>
+                            <label <?= isset($sexe) && $sexe == "femme" ? "checked" : "" ?> for="homme">Homme</label>
                         </div>
                         <div id="radio2">
-                            <input id="femme" type="radio" name="sexe"/>
+                            <input id="femme" type="radio" name="sexe" value="femme"/>
                             <label for="femme">Femme</label>
                         </div>
-                        <span id="fixSexe" class="inputError"></span>
+                        <span id="fixSexe" class="inputError"><?= $errors['sexe'] ?? "" ?></span>
                     </div>
 
                     <div id="date">
                         <label for="naissance"> Date de naissance </label>
-                        <input type="date" id="naissance" name="naissance" value="0">
-                        <span class="inputError"></span>
+                        <input type="date" id="naissance" name="naissance" value="<?= $naissance ?? "" ?>">
+                        <span class="inputError"><?= $errors['naissance'] ?? "" ?></span>
                     </div>
                     <div id="metier">
                         <select type="text" name="metier" class="input1">
                             <option selected disabled> Metier</option>
-                            <option> Etudiant </option>
-                            <option> Entrepeneur </option>
-                            <option> Cadre </option>
-                            <option> Salarié </option>
-                            <option> Sans emploi </option>
+                            <option <?= isset($metier) && $metier == "Etudiant" ? "selected" : ""?>> Etudiant </option>
+                            <option <?= isset($metier) && $metier == "Entrepeneur" ? "selected" : ""?>> Entrepeneur </option>
+                            <option <?= isset($metier) && $metier == "Cadre" ? "selected" : ""?>> Cadre </option>
+                            <option <?= isset($metier) && $metier == "Salarié" ? "selected" : ""?>> Salarié </option>
+                            <option <?= isset($metier) && $metier == "Sans emploi" ? "selected" : ""?>> Sans emploi </option>
                         </select>
-                        <span class="inputError"></span>
+                        <span class="inputError"><?= $errors['metier'] ?? "" ?></span>
                     </div>
                     <div id="sujet">
-                        <input type="text" name="sujet" class="input2" placeholder="Sujet"/>
-                        <span class="inputError"></span>
+                        <input type="text" name="sujet" class="input2" placeholder="Sujet" value="<?= $sujet ?? "" ?>"/>
+                        <span class="inputError"><?= $errors['sujet'] ?? "" ?></span>
                     </div>
                     <div id="message">
-                        <textarea type="text" name="message" placeholder="Message"></textarea>
-                        <span class="inputError"></span>
+                        <textarea type="text" name="message" placeholder="Message"><?= $message ?? "" ?></textarea>
+                        <span class="inputError"><?= $errors['message'] ?? "" ?></span>
                     </div>
 
                     <input type="submit" value="Contacter">
